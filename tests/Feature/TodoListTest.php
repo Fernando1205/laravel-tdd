4a40d22c2f2739270b13bf970c357a6e21e01d8d
Fernando Lopez <fer68769@gmail.com>
2022-06-16 12:34:13 -0500

    Test todo list all with factory

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -2,15 +2,19 @@
 
 namespace Tests\Feature;
 
+use App\Models\TodoList;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_index_todo_list()
     {
         // preparation / prepare
+        $list = TodoList::factory()->create();
 
         // action / perform
         $response = $this->getJson(route('todo-list.index'));
